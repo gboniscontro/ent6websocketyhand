a9b48edd4915ddafd42b68a77faaa667bbcef4b1
Gustavo Boniscontro <guscaballito@gmail.com>
2022-06-24 16:55:03 -0300

    Entrega 6

--- handlebars/src/index.js
+++ handlebars/src/index.js
@@ -1,29 +1,34 @@
-const express = require('express')
-const { Contenedor, Producto } = require('./contenedor')
-
-const path = require('path')
-let contenedor = new Contenedor('productos.txt')
-
-
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const app = express();
 
+const { Contenedor, Producto } = require('./contenedor')
+const path = require('path')
 
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+let contenedor = new Contenedor('productos.txt')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
-
 //Engine
 app.set('view engine', 'hbs');
-app.set("views",path.join(__dirname, 'views') );
+app.set("views", path.join(__dirname, 'views'));
+
 
 app.post('/productos', (req, res) => {
     const producto = req.body
     contenedor.save(producto);
+    let productos = contenedor.getAll();
+    io.sockets.emit('products', productos);
     res.redirect('/')
 })
+/*
 app.get('/productos', (req, res) => {
-    
+
     let productos = contenedor.getAll()
 
     res.render("index", {
@@ -31,7 +36,59 @@ app.get('/productos', (req, res) => {
         productosExists: productos.length
     });
 });
+app.get('api/productos', (req, res) => {
 
-app.listen(8080,()=>{
-    console.log(`Escuchando en el puerto 8080`);
-});
+    let productos = contenedor.getAll()
+
+    return res.send(productos)
+});*/
+
+
+
+
+
+app.use(express.static('public'));
+
+let messages = [
+    { author: 'juan@gmail.com', text: '!hola! que tal?' },
+    { author: 'jose@gmail.com', text: '!muy bien' },
+    { author: 'Ana@gmail.com', text: '!Genial!' }
+];
+
+
+io.on('connection', function (socket) {
+    console.log('un usuario se ha conectado');
+    let productos = contenedor.getAll()
+    socket.emit('products', productos);
+
+    socket.on('new-product', function (data) {
+        contenedor.save(data);
+        let productos = contenedor.getAll()
+        io.sockets.emit('products', productos);
+    })
+
+    console.log('emitimos chat');
+    socket.emit('messages', messages);
+
+    socket.on('new-message', function (data) {
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 8080;
+
+const serv = server.listen(PORT, () => {
+    console.log('listening on port', serv.address().port);
+})
+serv.on('error', err => console.error('listening on port', err))
